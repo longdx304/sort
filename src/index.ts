314b513eee9135755b5b8ded62d9562296da957d
Long Do <longdo@Longs-MacBook-Pro.local>
2022-01-14 18:56:43 +0700

    added support for linked list

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,5 @@
-import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
+// import { CharactersCollection } from './CharactersCollection';
 // import { NumbersCollection } from './NumbersCollection';
 import { Sorter } from './Sorter';
 
@@ -7,7 +8,16 @@ import { Sorter } from './Sorter';
 // sorter.sort();
 // console.log(numbersCollection.data);
 
-const charactersCollection = new CharactersCollection('Xaayb');
-const sorter = new Sorter(charactersCollection);
+// const charactersCollection = new CharactersCollection('Xaayb');
+// const sorter = new Sorter(charactersCollection);
+// sorter.sort();
+// console.log(charactersCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+const sorter = new Sorter(linkedList);
 sorter.sort();
-console.log(charactersCollection.data);
+linkedList.print();
